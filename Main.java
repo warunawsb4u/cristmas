047f7a0474b8419830caeee5e7829903ca5a6009
warunawsb4u <warunawsb4u@gmail.com>
2022-12-26 02:40:32 +0000

    commit Main java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Prints "Hello, World" in the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
